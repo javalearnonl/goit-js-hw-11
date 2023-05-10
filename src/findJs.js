cc080b74226d59f6ae88b639c4fd9f21d1a82f4f
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-05-11 01:30:41 +0300

    fin

---
+++ src/findJs.js
@@ -0,0 +1,79 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '36274489-574cd7b2e6e5eaf2239dbc903';
+
+export default class ImageFinder {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    try {
+      const imagesSet = await axios
+        .get(
+          `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`,
+        )
+        .then(response => {
+          const data = response.data;
+
+          if (data.hits.length === 0) {
+            this.notifyIncorrectQuery();
+          }
+          if (this.page * 40 >= data.totalHits && data.hits.length !== 0) {
+            this.notifyEndOfGallery();
+          }
+          if (this.page === 1 && data.totalHits !== 0) {
+            this.showAmountOfHits(data.totalHits);
+          }
+
+          this.incrementPage();
+          return data;
+        });
+      return imagesSet;
+    } catch (error) {
+      this.notifyQueryError(error);
+      console.log(error);
+    }
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  notifyIncorrectQuery() {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+      {
+        position: 'right-top',
+      },
+    );
+  }
+
+  notifyEndOfGallery() {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.", {
+      position: 'right-top',
+    });
+  }
+
+  showAmountOfHits(totalHits) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`, {
+      position: 'right-top',
+    });
+  }
+
+  notifyQueryError(error) {
+    Notiflix.Notify.failure(
+      `Oops! Something went wrong. You caught the following error: ${error.message}.`,
+      {
+        position: 'right-top',
+      },
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,134 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Search image:)</title>
-  </head>
-  <body>
-    <form class="search-form" id="search-form">
-      <input
-        type="text"
-        name="searchQuery"
-        autocomplete="off"
-        placeholder="Search images..."
-      />
-      <button type="submit">Find</button>
-    </form>
+import './css/styles.css';
+import ImageFinder from './findJs.js';
+import Notiflix from 'notiflix';
 
-    <script type="module" src="./index.js"></script>
-  </body>
-</html>
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchFormElement = document.getElementById('search-form');
+const imageGalleryElement = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+const sloganElements = document.querySelector('.app-slogan');
+
+const imageFinder = new ImageFinder();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
+
+searchFormElement.addEventListener('submit', handleSearchSubmit);
+loadMoreButton.addEventListener('click', handleLoadMoreClick);
+
+async function handleSearchSubmit(event) {
+  event.preventDefault();
+
+  resetMarkup();
+
+  const searchName = event.currentTarget.elements.searchQuery.value.trim();
+  imageFinder.searchQuery = searchName;
+
+  if (searchName !== '') {
+    imageFinder.resetPage();
+    const imgDataSet = await imageFinder.fetchImages();
+
+    if (imgDataSet.hits.length > 0) {
+      hideSlogan();
+    }
+
+    renderImgGallery(imgDataSet);
+
+    if (imgDataSet.hits.length < imgDataSet.totalHits) {
+      showLoadBtn();
+    }
+  }
+
+  if (searchName === '') {
+    notifySearchNameAbsence();
+    showSlogan();
+  }
+}
+
+async function handleLoadMoreClick() {
+  const nextImgDataSet = await imageFinder.fetchImages();
+  renderImgGallery(nextImgDataSet);
+
+  if ((imageFinder.page - 1) * 40 >= nextImgDataSet.totalHits) {
+    hideLoadBtn();
+  }
+}
+
+function renderImgGallery(data) {
+  const imgCards = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+ <a class="photo-card__item" href="${largeImageURL}"> 
+ <div class="photo-card__tumb">
+ <img class="photo-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </div>
+  <div class="info">
+  <p class="info-item"> <b class="info-item__param">👍</b> <span class="info-item__num">${likes} |</span> </p> 
+  <p class="info-item"> <b class="info-item__param">👀</b> <span class="info-item__num">${views} | </span> </p>
+   <p class="info-item"> <b class="info-item__param">✍️</b> <span class="info-item__num">${comments} | </span> 
+   </p> <p class="info-item"> <b class="info-item__param">💾</b> <span class="info-item__num">${downloads}</span> </p>
+    </div> </a> </div>`
+    )
+    .join('');
+
+  imageGalleryElement.insertAdjacentHTML('beforeend', imgCards);
+
+  lightbox.refresh();
+
+  makeSmoothScroll();
+}
+
+function resetMarkup() {
+  if (imageGalleryElement.childNodes.length !== 0) {
+    imageGalleryElement.innerHTML = '';
+    hideLoadBtn();
+    showSlogan();
+  }
+}
+
+function hideLoadBtn() {
+  loadMoreButton.classList.add('js-hidden');
+}
+
+function showLoadBtn() {
+  loadMoreButton.classList.remove('js-hidden');
+}
+
+function hideSlogan() {
+  sloganElements.classList.add('js-hidden');
+}
+
+function showSlogan() {
+  sloganElements.classList.remove('js-hidden');
+}
+
+function notifySearchNameAbsence() {
+  Notiflix.Notify.failure('For begin searching u need to input text', {
+    position: 'right-top',
+  });
+}
+
+function makeSmoothScroll() {
+  if (imageFinder.page - 1 > 1) {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 1.72,
+      behavior: 'smooth',
+    });
+  }
+}
